f9e05cb5a074e4f4bf43f8a5f2e4a06a11e6ec17
Archie Sua <archiesua@gmail.com>
2023-02-26 23:05:52 +1000

    update the Product-Service to integrate the new routing path based on cat-id

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,8 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number) : Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products));
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?=${theCategoryId}`
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(map(response => response._embedded.products));
   }
 }
 
