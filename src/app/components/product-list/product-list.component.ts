cf8a9b2e6d38dce468b3d127c1ce8c9deed31d35
Archie Sua <archiesua@gmail.com>
2023-03-04 01:04:00 +1000

    add event handler or binding to the cart button in the product-list component and create a method addToCart under the product-list component with console logging to test it works while logic is developed later

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -105,4 +105,9 @@ export class ProductListComponent implements OnInit {
     };
   }
 
+  addToCart(theProduct: Product)
+  {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
+
 }
