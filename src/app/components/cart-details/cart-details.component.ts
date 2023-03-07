8bab377bdf25d222c62316aa858ae0b8f4f94608
Archie Sua <archiesua@gmail.com>
2023-03-07 22:16:50 +1000

    add the decrement logic and event binding

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -38,6 +38,10 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.addToCart(theCartItem);
   }
 
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem)
+  }
+
 }
 
 
