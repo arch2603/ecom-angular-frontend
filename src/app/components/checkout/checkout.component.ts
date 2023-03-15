86a050a444dc057c0f8df2e2966a13bcb6e2217c
Archie Sua <archiesua@gmail.com>
2023-03-16 00:17:41 +1000

    add getter methods for the validators

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -39,8 +39,8 @@ export class CheckoutComponent implements OnInit {
       shippingAddress: this.formBuilder.group({
         street: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
         city: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
-        state: [''],
-        country: [''],
+        state: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
         zipCode: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
@@ -163,4 +163,14 @@ export class CheckoutComponent implements OnInit {
   get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
 
   get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
 }
