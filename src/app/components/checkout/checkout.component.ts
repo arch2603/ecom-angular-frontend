a93625966760223c89627b335cdcaa956bbf7943
Archie Sua <archiesua@gmail.com>
2023-03-22 20:41:05 +1000

    integrate  okta authenticated server for oauth2 authentication - adding config file and setting up a account

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Country } from 'src/app/common/country';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
@@ -27,7 +32,9 @@ export class CheckoutComponent implements OnInit {
 
   constructor( private formBuilder: FormBuilder, 
                private luv2ShopFormService: Luv2ShopFormService, 
-               private cartService: CartService) { }
+               private cartService: CartService,
+               private checkoutService: CheckoutService,
+               private router: Router) { }
 
   ngOnInit(): void {
 
@@ -125,11 +132,80 @@ export class CheckoutComponent implements OnInit {
 
     if (this.checkoutFormGroup.invalid) {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
+    //setup order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    //get cart items
+    const cartItems = this.cartService.cartItems;
+    //long-way
+    /* let orderItems: OrderItem[] = [];
+    // for(let i=0; cartItems.length; i++) {
+    //   orderItems[i] = new OrderItem(cartItems[i]);
+    } */
+
+    //short-way
+    //create orderItems form cartItems
+
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem))
+
+    //setup purchase
+    let purchase = new Purchase();
+
+    //populate purcahse - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+    //populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress!.state));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress!.country));
+    purchase.shippingAddress!.state = shippingState.name;
+    purchase.shippingAddress!.country = shippingCountry.name; 
+
+    //populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress!.state));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress!.country));
+    purchase.billingAddress!.state = billingState.name;
+    purchase.billingAddress!.country = billingCountry.name; 
+
+    //populate purchase - order and orderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    //call REST API via the CheckoutService
+    this.checkoutService.placeOrder(purchase).subscribe(
+      {
+        next: response => {
+          alert(`Your order has been received. \nOrder tracking numner: ${response.orderTrackingNumber}`);
+          this.resetCart();
+
+        },
+        error: err =>{
+          alert(`There was an error: ${err.message}`);
+        } 
+        
+      }
+    );
 
     console.log("Handling the submit button");
-    console.log(this.checkoutFormGroup.get('customer')!.value);
-    console.log("The email address is " + this.checkoutFormGroup.get('customer')!.value.email);
+    // console.log(this.checkoutFormGroup.get('customer')!.value);
+    // console.log("The email address is " + this.checkoutFormGroup.get('customer')!.value.email);
+  }
+
+  resetCart() {
+    //reset the cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    //reset the form
+    this.checkoutFormGroup.reset();
+
+    //navigate to the products page
+    this.router.navigateByUrl("/products");
   }
 
   handleMonthsAndYears() {
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa8ta0qm9N6ucYWt5d7',
+        issuer: 'https://dev-70627418.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid' , 'profile', 'email']
+    }
+}
